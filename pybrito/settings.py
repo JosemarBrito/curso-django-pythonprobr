7c79fadfc0eacaf0aae41cd2074b188ab3ccc28f
JosemarBrito <estudos.josemarbrito@gmail.com>
2020-10-20 22:20:06 -0300

    Configurado Python Decouple
    Close #7

--- pybrito/settings.py
+++ pybrito/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%hik+-%&ejon^xn)snk@booxf!0w%4bt8$gc@k1$xs+h-y+nv%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
